0a63144b8581410771aa30397ad71ba0f83d398f
Leonardo Gobetti <leonardogobetti@hotmail.com>
2024-04-26 13:46:00 -0300

    Exercícios de fila e Pilha

--- ExercicioFila.java
+++ ExercicioFila.java
@@ -10,6 +10,7 @@ public class ExercicioFila {
 	public static void main(String[] args) {
 
 		Queue<String> clientes = new LinkedList<String>();
+		Queue<String> clientesPrioritarios = new LinkedList<String>();
 
 		Scanner leia = new Scanner(System.in);
 		int opcao;
@@ -23,6 +24,7 @@ public class ExercicioFila {
 			System.out.println("      0 - Sair                               ");
 			System.out.println("*********************************************");
 			System.out.print("Entre com a opção desejada: ");
+			
 			opcao = leia.nextInt();
 			System.out.println();
 			
@@ -34,26 +36,57 @@ public class ExercicioFila {
 				System.out.print("Digite o Nome: ");
 				leia.nextLine();
 				String nome = leia.nextLine();
-				clientes.add(nome);
-				System.out.println("Fila: ");
-				System.out.println(clientes);
+				System.out.print("Digite a idade: ");
+				int idade = leia.nextInt();
+				System.out.println();
+				if(idade >= 60) {
+					clientesPrioritarios.add(nome);
+				}else {
+					clientes.add(nome);
+				}
+				if(clientes.isEmpty() == true) {
+					System.out.println("Fila:");
+					System.out.println("Fila vazia!");
+				}else {
+					System.out.println("Fila: ");
+					System.out.println(clientes);
+				}
+				if(clientesPrioritarios.isEmpty() == true) {
+					System.out.println("Fila Preferencial:");
+					System.out.println("Vazia:");
+				}else {
+					System.out.println("Fila Preferencial:");
+					System.out.println(clientesPrioritarios);
+				}
+				
 				System.out.println("Cliente Adicionado!");
 				break;
 			case 2:
 				System.out.println("Lista de Clientes da Fila:");
 				System.out.println(clientes);
+				System.out.println("Lista de Clientes da Fila Preferencial:");
+				System.out.println(clientesPrioritarios);
 				break;
 			case 3:
 				System.out.println("Fila:");
-				if (clientes.isEmpty() == true) {
-					System.out.println("A Fila está vazia!");
-				} else {
-					clientes.poll();
+				if (clientes.isEmpty() == true && clientesPrioritarios.isEmpty() == true) {
+					System.out.println("As Filas estão vazias!");
+				} else if(clientesPrioritarios.size() > clientes.size()) {
+					clientesPrioritarios.poll();
 					System.out.println(clientes);
+					System.out.println("Fila Preferencial:");
+					System.out.println(clientesPrioritarios);
 					System.out.println("O Cliente foi Chamado!");
+				}else if(clientesPrioritarios.size() <= clientes.size()) {
+					clientesPrioritarios.poll();
+					clientes.poll();
+					System.out.println("Os Clientes foram Chamados!");
+					
 				}
 				break;
-
+            default:
+            	System.out.println("Entre com uma opção válida!");
+            	break;
 			}
 		}while (opcao != 0);
 		
